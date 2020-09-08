d24a6ed3468a5f1c721b653bb22378690b586f35
sagar805 <koiralasagar138@gmail.com>
2020-09-08 20:53:48 +0545

    fits commit

---
+++ contacts/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+import smtplib
+from .models import Contact
+# Create your views here.
+def contact(request):
+    if request.method=="POST":
+        listing_id=request.POST['listing_id']
+        listing=request.POST['listing']
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        message=request.POST['message']
+        user_id=request.POST['user_id']
+        realtor_email=request.POST['realtor_email']
+
+      #Check if enquiry is made
+        if request.user.is_authenticated:
+            user_id=request.user.id
+            has_contacted= Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request,"You have already made an enquiry for this listing")
+                return redirect('/listings/'+listing_id)   
+        contact= Contact(listing_id=listing_id,listing=listing,name=name,email=email,
+        phone=phone,message=message,user_id=user_id)
+        contact.save()
+        #send mail
+        # send_mail(
+        #       'Property listing inquiry',
+        #       'There has been an inquiry for' + listing + '.Sign onto admin panel for more info',
+        #       'koiralasagar92@gmail.com',
+        #       [realtor_email,'koiralasagar138@gmail.com'],
+        #       fail_silently=False
+
+        #  )
+        messages.success(request,"Your request has been submitted and a realtor will return to you soon")
+        return redirect('/listings/'+listing_id)
---
+++ listings/choices.py
@@ -0,0 +1,35 @@
+city_choices={
+    'biratnagar':'biratnagar',
+    'kathmandu':'kathmandu',
+    'pokhara':'pokhara',
+    'damak':'damak',
+    'nepalgunj':'nepalgunj',
+    'birtamode':'birtamode',
+    'lumbini':'lumbini',
+    
+}
+bedroom_choices={
+      1: 1,
+      2: 2,
+      3: 3,
+      4: 4,
+      5: 5,
+      6: 6,
+      7: 7,
+      8: 8,
+      9: 9,
+     10: 10,
+ }
+price_choices={
+     '1000000':1000000,
+     '2000000':2000000,
+     '3000000':3000000,
+     '4000000':4000000,
+     '5000000':5000000,
+     '6000000':6000000,
+     '7000000':7000000,
+     '8000000':8000000,
+     '9000000':9000000,
+     '10000000':10000000,
+
+ }
